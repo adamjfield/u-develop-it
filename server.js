286fb7d0e1691df36e5ea76ec2dd985ddd9a9156
Adam Field <adamjfield@gmail.com>
2021-11-30 22:25:09 -0600

    add select,delete, and create queiries

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const db = mysql.createConnection(
     // Your MySQL username,
     user: "root",
     // Your MySQL password
-    password: "",
+    password: "password",
     database: "election",
   },
   console.log("Connected to the election database.")
@@ -24,11 +24,37 @@ db.query(`SELECT * FROM candidates`, (err, rows) => {
   console.log(rows);
 });
 
-// Default response for any other request (Not Found)
+// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(row);
+// });
+
+// Delete a candidate
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(result);
+// });
+
+// Create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) VALUES (?,?,?,?)`;
+const params = [1, "Ronald", "Firbank", 1];
+
+// db.query(sql, params, (err, result) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(result);
+// });
+
+//Default response for any other request (Not Found)
 app.use((req, res) => {
   res.status(404).end();
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port  http://localhost:${PORT}`);
 });
